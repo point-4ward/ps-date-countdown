327fae11bdf4c44df3a195530bd4e3caa4c87bf8
Cyro <github@cyrosy.fr>
2020-02-19 22:39:31 +0100

    fix: past events won't get negative year anymore (#10)

--- python_scripts/date_countdown.py
+++ python_scripts/date_countdown.py
@@ -12,25 +12,27 @@ dateMonth = int(dateSplit[1])
 dateYear =  int(dateSplit[2])
 date = datetime.date(dateYear , dateMonth , dateDay)
 
-thisYear = int(today.year)
-nextOccur = datetime.date(thisYear , dateMonth , dateDay)
+nextOccurYear = int(today.year)
+nextOccur = datetime.date(nextOccurYear , dateMonth , dateDay)
+
+if nextOccur < date:
+  # date must be the first occurrence
+  nextOccur = date
+
+if nextOccur < today:
+  # if event has passed this year, nextOccur is next year
+  nextOccurYear = nextOccurYear + 1
+  nextOccur = datetime.date(nextOccurYear, dateMonth, dateDay)
 
 numberOfDays = 0
-years = dateYear - thisYear
+years = nextOccurYear - dateYear
 
-# if the year is "next year" or further
-if years > 0:
-  nextOccur = datetime.date(dateYear , dateMonth , dateDay)
-  numberOfDays = (nextOccur - today).days
-else:
-  
-  numberOfDays = (nextOccur - today).days
-  # if event has passed this year
-  if numberOfDays < 0:
-    nextYear = thisYear + 1
-    nextOccur = datetime.date(nextYear , dateMonth , dateDay)
-    numberOfDays = (nextOccur - today).days
-    years = years + 1
+if years < 0:
+  # if years is negative, then date is more than 365 days away
+  # nextOccur will be the first occurrence
+  years = 0
+
+numberOfDays = (nextOccur - today).days
 
 defaultFriendlyName = ''
 
