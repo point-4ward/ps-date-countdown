0b3ba574f3a37112dd1dd1e87bbaf51a158a3996
Marc Forth <marcforth.social@gmail.com>
2020-05-15 10:06:11 +0100

    Handle Scandanavian characters (#13)

--- python_scripts/date_countdown.py
+++ python_scripts/date_countdown.py
@@ -1,9 +1,21 @@
-today = datetime.datetime.now().date()
+##########
+#
+# Date Countdown
+#
+# A python_script for homeassistant by mf_social
+#
+##########
 
+
+# Get the basic information to do the calculations
+today = datetime.datetime.now().date()
 name = data.get('name')
 eventType = data.get('type')
-sensorName = "sensor.{}_{}".format(eventType , name.replace(" " , "_"))
+defaultFriendlyName = ''
+numberOfDays = 0
 
+
+# Convert the date we got
 dateStr = data.get('date')
 dateSplit = dateStr.split("/")
 
@@ -12,10 +24,10 @@ dateMonth = int(dateSplit[1])
 dateYear =  int(dateSplit[2])
 date = datetime.date(dateYear , dateMonth , dateDay)
 
+
+# Calculate the next occurrence
 nextOccurYear = int(today.year)
 nextOccur = datetime.date(nextOccurYear , dateMonth , dateDay)
-
-defaultFriendlyName = ''
 
 if nextOccur < date:
   # date must be the first occurrence
@@ -26,7 +38,6 @@ if nextOccur < today:
   nextOccurYear = nextOccurYear + 1
   nextOccur = datetime.date(nextOccurYear, dateMonth, dateDay)
 
-numberOfDays = 0
 years = nextOccurYear - dateYear
 
 if years < 0:
@@ -36,11 +47,28 @@ if years < 0:
 
 numberOfDays = (nextOccur - today).days
 
+
+# Set the default friendly name
 if eventType.lower() == 'birthday':
+  # add an apostophe for birthdays
   defaultFriendlyName = "{}'s {}".format(name , eventType)
 else:
   defaultFriendlyName = "{} {}".format(name , eventType)
 
+
+# Sanitise the entity_id to meet the criteria by
+# replacing Scandanavian characters and spaces
+name1 = name.replace("Æ" , "AE")
+name2 = name1.replace("Ø" , "O")
+name3 = name2.replace("Å" , "AA")
+name4 = name3.replace("æ" , "ae")
+name5 = name4.replace("ø" , "o")
+name6 = name5.replace("å" , "aa")
+safeName = name6.replace(" " , "_")
+sensorName = "sensor.{}_{}".format(eventType , safeName)
+
+
+# Send the sensor to homeassistant
 hass.states.set(sensorName , numberOfDays ,
   {
     "icon" : data.get("icon", "mdi:calendar-star"),
